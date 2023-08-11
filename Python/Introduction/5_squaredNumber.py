b23a96bf3b2d148368907de16313f9cadc92312d
fredysetiawan26 <setiawannfredy@gmail.com>
2023-08-11 22:43:37 +0700

    update python (1-5)

---
+++ Python/Introduction/5_squaredNumber.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(0,n):
+        squared = i**2
+        print(squared)
---
+++ Python/Introduction/6_leapYear.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    # leap year condition:
+    # habis dibagi 4 
+    
+    return leap
+
+year = int(input())
